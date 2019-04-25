10c86a332cc6e6f0de3f11c6a5657757f03c163e
Saparbek28 <46023456+Saparbek28@users.noreply.github.com>
2019-04-25 22:35:44 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new AppContext())
+            {
+                var security = (from Security in context.Securities
+                                select Security).ToList();
+                if(security.Count > 0)
+                {
+                    foreach(var securitys in security)
+                    {
+                        Console.WriteLine($"Name: {securitys.Name}");
+                        Console.WriteLine($"Password: {securitys.Password}");
+                        Console.WriteLine($"Come in: {securitys.ComeIn.Hour}:{securitys.ComeIn.Minute}");
+                        Console.WriteLine($"Released: {securitys.Released.Hour}:{securitys.Released.Minute}");
+                        Console.WriteLine($"Target: {securitys.Target}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Oops no one here");
+                }
+            }
+            
+        }
+    }
+}
---
+++ Security.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    public class Security : Entity
+    {
+        public string Name { get; set; }
+        public DateTime ComeIn { get; set; } = new DateTime();
+        public DateTime Released { get; set; } = new DateTime();
+        public int Password { get; set; }
+        public string Target { get; set; }
+    }
+}
